0688ffb6e4f00d990c2947dcad93b85768cef1d4
Nicholas Youn <nicholas@Nicholasui-Air.hitronhub.home>
2018-02-24 10:43:36 -0800

    olympicflag

--- olympic.js
+++ olympic.js
@@ -3,8 +3,48 @@ window.onload = init;
 function init() {
     canvas = document.getElementById("mycanvas");
     ctx = canvas.getContext("2d");
+    // Background
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 1063,609);
+    ctx.fillRect(0, 0, 1063, 709);
+    // Rings
+    blue();
+    black();
+    red();
+    yellow();
+    green();
+}
+function blue() {
     ctx.beginPath();
-    ctx.arc
+    ctx.lineWidth = 20;
+    ctx.arc(275, 295, 115, 0, 2 * Math.PI);
+    ctx.strokeStyle="#2F85C2"
+    ctx.stroke();
+}
+function black() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(531, 295, 115, 0, 2 * Math.PI);
+    ctx.strokeStyle="black"
+    ctx.stroke();
+}
+function red() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(789, 295, 115, 0, 2 * Math.PI);
+    ctx.strokeStyle="#ED3A4E"
+    ctx.stroke();
+}
+function yellow() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(403, 413, 115, 0, 2 * Math.PI);
+    ctx.strokeStyle="#F6B143"
+    ctx.stroke();
+}
+function green() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(661, 413, 115, 0, 2 * Math.PI);
+    ctx.strokeStyle="#3E8B3D"
+    ctx.stroke();
 }
