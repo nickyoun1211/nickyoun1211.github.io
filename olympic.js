b3b213aa8c994d8cf2a8ada0b3c3a3ff23156e11
Nicholas Youn <nicholas@Nicholasui-Air.hitronhub.home>
2018-02-25 22:05:03 -0800

    ㅊㅗ뭏ㄷ

--- olympic.js
+++ olympic.js
@@ -1,3 +1,4 @@
+var canvas;
 var ctx;
 window.onload = init;
 function init() {
@@ -7,17 +8,66 @@ function init() {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 1063, 709);
     // Rings
-    blue();
-    black();
-    red();
-    yellow();
-    green();
+    // blue();
+    // black();
+    // red();
+    // yellow();
+    // green();
+    blueYellow1();
+    yellowblue1();
+    yellowblue2();
+    blueYellow2();
+    blackyellow1();
+    yellowblack1();
 }
 function blue() {
     ctx.beginPath();
     ctx.lineWidth = 20;
     ctx.arc(275, 295, 115, 0, 2 * Math.PI);
     ctx.strokeStyle="#2F85C2"
+    ctx.stroke();
+}
+function blueYellow1() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(275, 295, 115,Math.PI/4 , 5 * Math.PI/4);
+    ctx.strokeStyle="#2F85C2"
+    ctx.stroke();
+}
+    
+function blueYellow2() {
+        ctx.beginPath();
+        ctx.lineWidth = 20;
+        ctx.arc(275, 295, 115,5*Math.PI/4 , Math.PI/4);
+        ctx.strokeStyle="#2F85C2"
+        ctx.stroke();
+}
+function yellowblue1() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(403, 413, 115,Math.PI/4 , 5 * Math.PI/4) ;
+    ctx.strokeStyle="#F6B143"
+    ctx.stroke();
+}
+function yellowblue2() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(403, 413, 115, 5 * Math.PI/4, 3*Math.PI/2) ;
+    ctx.strokeStyle="#F6B143"
+    ctx.stroke();
+}
+function blackyellow1() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(531, 295, 115, 3*Math.PI/4, 7*Math.PI/4);
+    ctx.strokeStyle="black"
+    ctx.stroke();
+}
+function yellowblack1() {
+    ctx.beginPath();
+    ctx.lineWidth = 20;
+    ctx.arc(403, 413, 115, 3 * Math.PI/2,7*Math.PI/4);
+    ctx.strokeStyle="#F6B143"
     ctx.stroke();
 }
 function black() {
