a3da44d48e566fbad38d05633ba75c3d7a6fd3cc
Nicholas Youn <nicholas@Nicholasui-Air.hitronhub.home>
2018-03-16 20:56:46 -0700

    c

--- canvas.js
+++ canvas.js
@@ -1,23 +1,22 @@
+var requestID;
 var canvas;
 var ctx;
+var positionX=0
 window.onload = init;
-function init(){
+function init() {
     canvas = document.getElementById("mycanvas");
     ctx = canvas.getContext("2d");
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 500, 500);
+    ctx.fillStyle = "green";
+    startAnimation();
+}
+function startAnimation() {
+    requestID = requestAnimationFrame(animationLoop);
+}
+function animationLoop(timeStamp) {
 
-    ctx.save()
-    ctx.translate(150,250);
-    ctx.rotate(Math.PI /4);
-    ctx.fillStyle="red"
-    ctx.fillRect(0,0,50,100)
-    ctx.restore();
 
-    ctx.save()
-    ctx.translate(350,320);
-    ctx.rotate(3*Math.PI /4);
-    ctx.fillStyle="red"
-    ctx.fillRect(0,0,50,100)
-    ctx.restore();
+
+    requestID = requestAnimationFrame(animationLoop);
 }
