773ef64fc0a703908a499192104e562da960e6d2
Nicholas Youn <nicholas@Nicholasui-Air.hitronhub.home>
2018-03-16 20:37:29 -0700

    change

--- canvas.js
+++ canvas.js
@@ -6,13 +6,18 @@ function init(){
     ctx = canvas.getContext("2d");
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 500, 500);
-   drawBox(5,5);
-}
-function drawBox(x,y){
+
     ctx.save()
-    ctx.translate(x,y)
+    ctx.translate(150,250);
+    ctx.rotate(Math.PI /4);
     ctx.fillStyle="red"
-    ctx.fillRect(0,0,10,10)
+    ctx.fillRect(0,0,50,100)
     ctx.restore();
 
+    ctx.save()
+    ctx.translate(350,320);
+    ctx.rotate(3*Math.PI /4);
+    ctx.fillStyle="red"
+    ctx.fillRect(0,0,50,100)
+    ctx.restore();
 }
--- robot.js
+++ robot.js
@@ -6,7 +6,18 @@ function init(){
     ctx = canvas.getContext("2d");
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 792, 860);
-    drawCircle(189,441,25.5)
+    ctx.lineWidth=3;
+    ctx.strokeStyle ="black";
+    
+    drawCircle(552,162,24)
+    drawCircle(269,162,24)
+    drawCircle(411,75,24)
+
+    drawArm(290,310,40,170,Math.PI /4)
+    drawArm(166,454,40,92,20*Math.PI/11)
+    drawArm(500,350,40,160,7*Math.PI /4)
+    drawArm(635,460,40,92,15*Math.PI/12)
+
     drawRectangle(316,544,67,176)
     drawRectangle(429,544,67,176)
     drawRectangle(368,191,78,191);
@@ -25,10 +36,27 @@ function init(){
     drawRectangle(341,285,98,4)
     drawRectangle(341,305,98,4)
     drawRectangle(341,324,98,4)
+    drawCircle(463,120,24);
+    drawCircle(360,120,24);
+    drawCircle(492,288,9.5);
+    drawCircle(492,324.5,9.5);
+    drawCircle(492,361,9.5);
+    drawCircle(492,398,9.5);
+    drawCircle(492,361,9.5);
+    drawCircle(492,434.5,9.5);
+    drawCircle(492,471,9.5);
+    drawCircle(492,507.5,9.5);
+    drawCircle(492,544,9.5);
+    drawCircle(189,441,25.5);
+    drawCircle(620,440,23.5);
+    drawCircle(250,558,46.5)
+    drawCircle(708,348,46.5)
+
+    drawFeet(349,745,55);
+    drawFeet(470,745,55);
+    drawShoe();
 }
 function drawRectangle(x,y,w,h){
-    ctx.strokeStyle ="black";
-    ctx.lineWidth=3;
     ctx.save();
     ctx.translate(x,y)
     ctx.fillStyle="white"
@@ -38,12 +66,38 @@ function drawRectangle(x,y,w,h){
    
 }
 function drawCircle(x,y,r){
-    ctx.strokeStyle ="black";
-    ctx.lineWidth=3;
     ctx.save();
     ctx.translate(x,y)
     ctx.beginPath();
     ctx.arc(0,0,r,0,2*Math.PI)
+    ctx.fillStyle="white"
+    ctx.fill();
     ctx.stroke();
     ctx.restore();
 } 
+function drawArm(x,y,w,h,angle){
+    ctx.save();
+    ctx.translate(x,y)
+    ctx.rotate(angle);
+    ctx.strokeRect(0,0,w,h)
+    ctx.restore();
+}
+function drawFeet(x,y,r){
+    ctx.save();
+    ctx.translate(x,y)
+    ctx.beginPath();
+    
+    ctx.arc(0,0,r,Math.PI,0)
+    ctx.fillStyle="white"
+    ctx.fill();
+    ctx.lineWidth=10;
+    ctx.stroke();
+    ctx.restore();
+}
+function drawShoe(){
+    ctx.beginPath();
+    ctx.moveTo(289,749);
+    ctx.lineTo(530,749);
+    ctx.lineWidth=10;
+    ctx.stroke();
+}
